99de103a8146f24b1499d5aceec6e60515e9bfb3
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-02-19 23:45:40 +0000

    Variáveis inteiras

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+    
+        private static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            int a = 1;
+            int b = 1334;
+            int c = 839;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
