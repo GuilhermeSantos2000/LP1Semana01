b6fb47ed8b3917610633b8de21d12dd0c65da89e
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-02-20 21:30:09 +0000

    strings formatadas

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -7,6 +7,14 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
+
+            string x1 = $"{xx:f2}";
+            string x2 = $"{xx:p1}";
+            string i1 = $"{ii:x}";
+            string i2 = $"{ii:c}";
+
             string existence = "\"to be or not to be\" that is the question \u2753";
             string lyrics = "\" If you tried just a little more times, I would've made you a believer\" by Lisa \u2734";
             string quote ="\'I have a dream\' by Martin Luther King Jr. \u270B";
@@ -29,6 +37,11 @@ namespace ManyStrings
             Console.WriteLine(namewithage);
             Console.WriteLine(statement);
             Console.WriteLine(statement2);
+            
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+            Console.WriteLine(i1);
+            Console.WriteLine(i2);
         }
     }
 }
