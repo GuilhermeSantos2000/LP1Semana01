8cae4e4bd047e7adef1810a493c37286d61b99c1
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-02-20 21:19:14 +0000

    strings concatenadas e interpoladas

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace ManyStrings
 {
@@ -8,11 +9,26 @@ namespace ManyStrings
         {
             string existence = "\"to be or not to be\" that is the question \u2753";
             string lyrics = "\" If you tried just a little more times, I would've made you a believer\" by Lisa \u2734";
-            string quote ="\' I have a dream\' by Martin Luther King Jr. \u270B";
+            string quote ="\'I have a dream\' by Martin Luther King Jr. \u270B";
+
+            string name ="John";
+            string surname = "Doe";
+            string fullname = name + " " + surname;
+            string withmiddlename = name + " Diddly " + surname;
+            int age = 25;
+            string namewithage = fullname + " " + 25;
+            string statement = $"O sobrenome do {name} é {surname}";
+            string statement2 = $"A idade do {fullname} é {age}";
 
             Console.WriteLine(existence);
             Console.WriteLine(lyrics);
             Console.WriteLine(quote);
+
+            Console.WriteLine(fullname);
+            Console.WriteLine(withmiddlename);
+            Console.WriteLine(namewithage);
+            Console.WriteLine(statement);
+            Console.WriteLine(statement2);
         }
     }
 }
