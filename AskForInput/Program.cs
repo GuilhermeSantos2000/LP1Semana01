497f2be396e68a7a6daff6debe06e8c7e83a2484
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-02-20 21:37:23 +0000

    pedir input de número inteiro

---
+++ AskForInput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please write an integer.");
+            
+            string str = Console.ReadLine();
+            int a = int.Parse(str);
+        }
+    }
+}
