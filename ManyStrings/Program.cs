b0837e584fc618213af6d425b65de0331fb6e8eb
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-02-20 21:07:17 +0000

    Várias strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string existence = "\"to be or not to be\" that is the question \u2753";
+            string lyrics = "\" If you tried just a little more times, I would've made you a believer\" by Lisa \u2734";
+            string quote ="\' I have a dream\' by Martin Luther King Jr. \u270B";
+
+            Console.WriteLine(existence);
+            Console.WriteLine(lyrics);
+            Console.WriteLine(quote);
+        }
+    }
+}
