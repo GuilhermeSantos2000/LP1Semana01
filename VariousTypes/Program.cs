4948d12c39f525e286df7f225d34ab6424a49740
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-02-20 00:08:25 +0000

    Variáveis char

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -12,10 +12,18 @@ namespace VariousTypes
             int a = 1;
             int b = 1334;
             int c = 839;
+            
+            char smiley = '\u263A';
+            char omega = '\u03A9';
+            char heart = '\u2665';
 
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
+            
+            Console.WriteLine(smiley);
+            Console.WriteLine(omega);
+            Console.WriteLine(heart);
         }
     }
 }
