2c2c8b8c82557bb43754bc408d10389b31fccc99
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-02-21 18:01:44 +0000

    ex1

---
+++ Smiley/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere código :");
+            string a = Console.ReadLine();
+            int i = system.int32.parse(a, numberstyle.hexa)
+            
+            Console.WriteLine($"Caráter Unicode : {char.ConvertFromUtf32(i)}");
+        }
+    }
+}
